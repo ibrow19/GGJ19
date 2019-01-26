96bd7a7777683938de67f748164bec91372271d7
FergusBrown <43826955+FergusBrown@users.noreply.github.com>
2019-01-26 12:46:20 +0000

    Added main menu
    main menu controls with controller left stick. Made small edit to input mapping.

---
+++ Crab Snooker/Assets/Scripts/LoadSceneOnPress.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneOnPress : MonoBehaviour
+{
+    //Load scene by index
+    public void LoadByIndex(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
---
+++ Crab Snooker/Assets/Scripts/SelectOnInput.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SelectOnInput : MonoBehaviour
+{
+    public EventSystem eventSystem;
+    public GameObject selectedObject;
+
+    // boolean for if button selected, defaults to false
+    private bool buttonSelected;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if gamepad input detected and button not already selected
+        if (Input.GetAxisRaw("VerticalL") != 0 && !buttonSelected)
+        {
+            eventSystem.SetSelectedGameObject(selectedObject);
+            buttonSelected = true;
+        }
+        print(Input.GetAxisRaw("VerticalL"));
+    }
+
+    // When game object deactivated button no longer selected
+    private void OnDisable()
+    {
+        buttonSelected = false;
+    }
+}
