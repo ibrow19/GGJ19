ce7468d06a208c9b12f2002f11864869c5c1188e
FergusBrown <43826955+FergusBrown@users.noreply.github.com>
2019-01-26 22:50:34 +0000

    Ball art added

---
+++ Crab Snooker/Assets/Scripts/RandomSprite.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSprite : MonoBehaviour
+{
+    public Sprite otherSprite;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bool randomSelect = (Random.value > 0.5f);
+
+        if (randomSelect)
+            GetComponent<SpriteRenderer>().sprite = otherSprite;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
