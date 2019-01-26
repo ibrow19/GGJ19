78400685b373c6d374f926c7ddc2339acd58c556
FergusBrown <43826955+FergusBrown@users.noreply.github.com>
2019-01-26 00:03:12 +0000

    Stick test

---
+++ Crab Snooker/Assets/Rotate.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    //inputs
+    private float xValL;
+    private float yValL;
+    private float xValR;
+    private float yValR;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        xValR = Input.GetAxis("HorizontalR");
+        yValR = Input.GetAxis("VerticalR");
+
+        print("X dir Left: \n" + xValR);
+        //print("Y dir Left: \n" + yValR);
+    }
+}
