2e9e0fe7a523da1c0c6dd0cc5bce6138a329f03c
FergusBrown <43826955+FergusBrown@users.noreply.github.com>
2019-01-27 14:03:04 +0000

    fix

---
+++ Crab Snooker/Assets/Scripts/SelectOnInput2.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class SelectOnInput2 : MonoBehaviour
+{
+    public EventSystem eventSystem;
+    public GameObject selectedObject;
+
+
+    //public Button selectButton;
+
+    // Start is called before the first frame update
+    void OnEnable()
+    {
+        EventSystem EVRef = EventSystem.current; // get the current event system
+        EVRef.SetSelectedGameObject(this.gameObject);   // set current selected button
+                                                        //    Hack: Move up to the button above us (so we can hack a highlight on it)
+        Button bref = EVRef.currentSelectedGameObject.GetComponent<Button>();
+        EVRef.SetSelectedGameObject(bref.navigation.selectOnUp.gameObject, null);
+
+    }
+    
+
+
+}
