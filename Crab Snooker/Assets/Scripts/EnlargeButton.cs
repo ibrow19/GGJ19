0f117862a5450f1aaa5ea6363d89b014011524e3
FergusBrown <43826955+FergusBrown@users.noreply.github.com>
2019-01-27 12:11:40 +0000

    Menu and Title

---
+++ Crab Snooker/Assets/Scripts/EnlargeButton.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class EnlargeButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+
+    private Text myText;
+
+    void Start()
+    {
+        myText = GetComponentInChildren<Text>();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        myText.fontSize = 150;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        myText.fontSize = 100;
+        print("Deselected");
+    }
+}
